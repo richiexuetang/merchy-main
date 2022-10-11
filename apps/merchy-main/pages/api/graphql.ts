7978246f3bf11a82510fc15e42325832caf2fda9
richiexuetang <richard772794659@gmail.com>
2022-10-11 12:42:35 -0700

    change graphql endpoint

--- apps/merchy-main/pages/api/graphql.ts
+++ apps/merchy-main/pages/api/graphql.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://127.0.0.1:8000/graphql/',
+  uri: 'https://merchy.lol/graphql/',
   cache: new InMemoryCache(),
 });
 
